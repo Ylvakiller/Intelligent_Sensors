cb9593a680d88fa052d0ec77fdfdfa5abf8f4901
Ylvakiller <rem.geuze@gmail.com>
2014-11-19 15:28:28 +0100

    Got some start code from the net working
    I can now open and display an image

--- Programs/src/image/Opener.java
+++ Programs/src/image/Opener.java
@@ -1,5 +1,16 @@
 package image;
 
+import java.awt.BorderLayout;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
+
 /**
  * This class will be the class that opens a picture, stores that in memory and returns that.
  * for now only a stub, more documentation will come soon
@@ -7,5 +18,55 @@ package image;
  *
  */
 public class Opener {
+	String fileName = "./InputImages/plate02.jpg";
+	
+	/**
+	 * Default Constructor
+	 */
+	public Opener(){
+		
+	}
+	
+	/**
+	 * Constructor with a file name in it.
+	 * @param fileName
+	 */
+	public Opener(String fileName){
+		this.fileName = fileName;
+	}
+	
+	/**
+	 * Opens an image and displays that on a screen
+	 * @throws Exception this is an ImageIO exception, for example when the program cannot read or find the file
+	 */
+	public void ImageDemo() throws Exception
+	{
+		SwingUtilities.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				JFrame editorFrame = new JFrame("Image Demo");
+				editorFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
+				BufferedImage image = null;
+				try
+				{
+					image = ImageIO.read(new File(fileName));
+				}
+				catch (Exception e)
+				{
+					e.printStackTrace();
+					System.exit(1);
+				}
+				ImageIcon imageIcon = new ImageIcon(image);
+				JLabel jLabel = new JLabel();
+				jLabel.setIcon(imageIcon);
+				editorFrame.getContentPane().add(jLabel, BorderLayout.CENTER);
+
+				editorFrame.pack();
+				editorFrame.setLocationRelativeTo(null);
+				editorFrame.setVisible(true);
+			}
+		});
+	}
 }
--- Programs/src/main/Runner.java
+++ Programs/src/main/Runner.java
@@ -1,5 +1,7 @@
 package main;
 
+import image.Opener;
+
 /**
  * This is the class that will start the program, uses for it are debugging and in the end.
  * @author Ylva
@@ -9,7 +11,13 @@ public class Runner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Opener test = new Opener();
+		try {
+			test.ImageDemo();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
