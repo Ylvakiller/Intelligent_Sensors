e05d88c4d7341166e24cdabdc18cb87b490e2e2b
Ylvakiller <rem.geuze@gmail.com>
2014-11-19 14:59:41 +0100

    Started the java program for this assignment

---
+++ Programs/src/image/Opener.java
@@ -0,0 +1,11 @@
+package image;
+
+/**
+ * This class will be the class that opens a picture, stores that in memory and returns that.
+ * for now only a stub, more documentation will come soon
+ * @author Ylva
+ *
+ */
+public class Opener {
+
+}
---
+++ Programs/src/main/Runner.java
@@ -0,0 +1,15 @@
+package main;
+
+/**
+ * This is the class that will start the program, uses for it are debugging and in the end.
+ * @author Ylva
+ *
+ */
+public class Runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
