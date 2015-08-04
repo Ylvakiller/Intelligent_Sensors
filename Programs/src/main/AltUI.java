681855a81af5fadeaf553465e2d632520fda0da6
Ylvakiller <rem.geuze@gmail.com>
2015-08-04 22:12:55 +0200

    Started with the sidePanel
    Made all the panels show up correctely using etchedBorders, split the
    sidePanel up into a buttonArea and a textArea on which I can dump text
    :)

--- Programs/src/main/AltUI.java
+++ Programs/src/main/AltUI.java
@@ -2,6 +2,9 @@ package main;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.border.EtchedBorder;
 
 /*
  * This class will hold my attempt at making a multi treaded interface for the program, showing to start 4 numberplate at the same time.
@@ -10,61 +13,72 @@ public class AltUI {
 	/**
 	 * This constructor will start and manage the complete interface
 	 */
+	public static JTextArea menuOutput;
 	
 	public AltUI() {
+		//This is the top container, anything that has directely to do with thise container will be on this indentation level
 		JFrame topContainer = new JFrame("Experimental interface for the numberplate recogniser");
 		topContainer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		topContainer.setSize(1920,1080);
 		topContainer.getContentPane().setLayout(null);
-		
-		/*JPanel panel = new JPanel();
-		panel.setBounds(0, 0, 233, 802);
-		topContainer.getContentPane().add(panel);
-		*/
-		JPanel sidePanel = new JPanel();
-		sidePanel.setBounds(5, 5, 275, 1030);
-		topContainer.getContentPane().add(sidePanel);
-		sidePanel.setLayout(null);
-		
-		JPanel plateContainer = new JPanel();
-		plateContainer.setBounds(290, 50, 1610, 960);
-		topContainer.getContentPane().add(plateContainer);
-		plateContainer.setLayout(null);
-		
-		JPanel plate1 = new JPanel();
-		plate1.setBounds(0, 0, (plateContainer.getWidth()-10)/2, (plateContainer.getHeight()-10)/2);
-		plateContainer.add(plate1);
-		
-		JPanel plate2 = new JPanel();
-		plate2.setBounds((plateContainer.getWidth()-10)/2+10, 0, (plateContainer.getWidth()-10)/2, (plateContainer.getHeight()-10)/2);
-		plateContainer.add(plate2);
-		
-		JPanel plate3 = new JPanel();
-		plate3.setBounds(0, (plateContainer.getHeight()-10)/2+10, (plateContainer.getWidth()-10)/2, (plateContainer.getHeight()-10)/2);
-		plateContainer.add(plate3);
-		
-		JPanel plate4 = new JPanel();
-		plate4.setBounds((plateContainer.getWidth()-10)/2+10, (plateContainer.getHeight()-10)/2+10, (plateContainer.getWidth()-10)/2, (plateContainer.getHeight()-10)/2);
-		plateContainer.add(plate4);
-		
-		
-		
-		
-		plate1.setVisible(true);
-		plate2.setVisible(true);
-		plate3.setVisible(true);
-		plate4.setVisible(true);
-		plateContainer.setVisible(true);
-		sidePanel.setVisible(true);
-		topContainer.setVisible(true);
-		
-		
-		
-		
-		while (true){
-			System.out.println("width: " + topContainer.getWidth());
+		{
+			// Up next is the side panel, this will include all the buttons and actionlisteners
+			JPanel sidePanel = new JPanel();
+			sidePanel.setBounds(5, 5, 275, 1030);
+			topContainer.getContentPane().add(sidePanel);
+			sidePanel.setLayout(null);
+			{
+				JPanel buttonPanel = new JPanel();
+				buttonPanel.setBounds(0, 0, sidePanel.getWidth(), 275);
+				buttonPanel.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null,null));
+				sidePanel.add(buttonPanel);
 
-			System.out.println("Height: " + topContainer.getHeight());
+				JPanel textPanel = new JPanel();
+				textPanel.setBounds(0, buttonPanel.getHeight(), sidePanel.getWidth(), sidePanel.getHeight()-buttonPanel.getHeight());
+				textPanel.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null,null));
+				sidePanel.add(textPanel);
+				{
+					JScrollPane scrollPane = new JScrollPane();
+					scrollPane.setBounds(0, 0, textPanel.getWidth(), textPanel.getHeight());
+					scrollPane.setAutoscrolls(true);
+					menuOutput = new JTextArea();
+					scrollPane.setViewportView(menuOutput);
+					menuOutput.setLineWrap(true);
+					
+				}
+			}
+
+			//This is a container which will hold all the different plates, at the time of this writing (4-8-2015) this is meant for a 2x2 grid of numberplates
+			JPanel plateContainer = new JPanel();
+			plateContainer.setBounds(290, 50, 1610, 960);
+			topContainer.getContentPane().add(plateContainer);
+			plateContainer.setLayout(null);
+			{
+				//These are all seperate plates
+				JPanel plate1 = new JPanel();
+				plate1.setBounds(0, 0, (plateContainer.getWidth()-10)/2, (plateContainer.getHeight()-10)/2);
+				plate1.setBorder(new EtchedBorder(EtchedBorder.RAISED, null,null));
+				plateContainer.add(plate1);
+
+				JPanel plate2 = new JPanel();
+				plate2.setBounds((plateContainer.getWidth()-10)/2+10, 0, (plateContainer.getWidth()-10)/2, (plateContainer.getHeight()-10)/2);
+				plate2.setBorder(new EtchedBorder(EtchedBorder.RAISED, null,null));
+				plateContainer.add(plate2);
+
+				JPanel plate3 = new JPanel();
+				plate3.setBounds(0, (plateContainer.getHeight()-10)/2+10, (plateContainer.getWidth()-10)/2, (plateContainer.getHeight()-10)/2);
+				plate3.setBorder(new EtchedBorder(EtchedBorder.RAISED, null,null));
+				plateContainer.add(plate3);
+
+				JPanel plate4 = new JPanel();
+				plate4.setBounds((plateContainer.getWidth()-10)/2+10, (plateContainer.getHeight()-10)/2+10, (plateContainer.getWidth()-10)/2, (plateContainer.getHeight()-10)/2);
+				plate4.setBorder(new EtchedBorder(EtchedBorder.RAISED, null,null));
+				plateContainer.add(plate4);
+			}
+		}
+		topContainer.setVisible(true);
+		while (true){
+
 		}
 	}
 }
